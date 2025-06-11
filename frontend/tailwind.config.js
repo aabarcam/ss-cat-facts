59e0ab839df3a1704274a1ffe4a5f8bd08b2efb4
aabarcam <aeduardo10x@gmail.com>
2025-06-11 18:37:14 -0400

    chore: dependencies config files

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,20 @@
+// tailwind.config.js
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  content: [
+    // ... your project files, eg.:
+    // './index.html',
+    // './src/**/*.{vue,js,ts,jsx,tsx}',
+    './vueform.config.js', // or where `vueform.config.js` is located. Change `.js` to `.ts` if required.
+    './node_modules/@vueform/vueform/themes/tailwind/**/*.vue',
+    './node_modules/@vueform/vueform/themes/tailwind/**/*.js',
+  ],
+  darkMode: 'class',
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('@vueform/vueform/tailwind'),
+  ]
+}

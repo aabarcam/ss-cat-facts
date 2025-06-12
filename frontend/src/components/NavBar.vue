46a599ba64b2ba870f6e84a8c4f5026c421f9e40
aabarcam <aeduardo10x@gmail.com>
2025-06-12 01:57:20 -0400

    feat: add navbar

---
+++ frontend/src/components/NavBar.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+     <nav>
+    <RouterLink to="/" class="nav-link">SSCatFacts</RouterLink>
+    <RouterLink to="/login" v-if="!authenticated" class="nav-link">Login</RouterLink>
+    <RouterLink to="/register" v-if="!authenticated" class="nav-link">Register</RouterLink>
+    <RouterLink to="/facts" v-if="authenticated" class="nav-link">Cat Facts</RouterLink>
+    <RouterLink to="/likes" v-if="authenticated" class="nav-link">My likes</RouterLink>
+    <RouterLink to="/popular" v-if="authenticated" class="nav-link">Popular facts</RouterLink>
+    <button @click="logout()" v-if="authenticated" class="nav-link">Log out</button>
+    </nav>
+  </div>
+</template>
+
+<script setup>
+
+let authenticated = localStorage.getItem("jwt")
+
+function logout() {
+  localStorage.removeItem("jwt")
+  window.location.href = "/"
+}
+
+</script>
+
+<style scoped src="../styles.css">
+</style>
